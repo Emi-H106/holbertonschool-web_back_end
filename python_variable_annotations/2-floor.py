f4bdfaa49ca8ac4c39ed9b151ea21dbf10b033c5
Emi-H106 <emi.hatano@yahoo.com>
2025-07-29 15:27:00 +0200

    add 2-floor.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module to floor a floating-point number with type annotations.
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Floor a floating-point number.
+    """
+    return math.floor(n)
