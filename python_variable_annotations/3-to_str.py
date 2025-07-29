92b891f10f0b4a7e25d82eff6ffde2c9e3ccf0ee
Emi-H106 <emi.hatano@yahoo.com>
2025-07-29 15:31:13 +0200

    add 3-to_str.py

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module to convert a floating-point number to a string with type annotations.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a floating-point number to a string.
+    """
+    return str(n)
