860eb1749a7faaded8baa0383ea37d256eb0702a
Emi-H106 <emi.hatano@yahoo.com>
2025-07-30 10:30:03 +0200

    add  type-annotated

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,9 +4,10 @@ Concurrent coroutines example using asyncio and random module.
 """
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     Asynchronously generates random floats between 0 and 10.
     Yields 10 random floats, each after a 1 second delay.
