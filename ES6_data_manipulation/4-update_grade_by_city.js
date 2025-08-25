e603acbd2e42fb14b1e3371913ad7e065ac9324f
Emi-H106 <emi.hatano@yahoo.com>
2025-08-25 15:11:21 +0200

    add 4-update_grade_by_city.js

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,19 @@
+export default function updateStudentGradeByCity(students, city, newGrade) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const updatedStudent = newGrade.find(g => g.studentId === student.id);
+      let grade;
+      if (updatedStudent) {
+        grade = updatedStudent.grade;
+      } else {
+        grade = 'N/A';
+      }
+
+      return {
+        ...student,
+        grade: grade
+      };
+    })
+
+}
