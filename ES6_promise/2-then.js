20bc4b278670dd7da633c442d15f0db0ad404116
Emi-H106 <emi.hatano@yahoo.com>
2025-08-27 10:25:55 +0200

    fix success

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,7 +2,7 @@
 export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => { 
-        return {status: 200, body: "Success" };
+        return {status: 200, body: "success" };
     })
     .catch(() => {
         return new Error();
