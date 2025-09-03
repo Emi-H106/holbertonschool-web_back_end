c16c8b7251680d7d41935d59f0ebfe3f0c879d07
Emi-H106 <emi.hatano@yahoo.com>
2025-09-03 16:25:54 +0200

    fix code for stdout.write

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,8 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf-8');
-process.stdin.on('data', (input) => {
-  const name = input.trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data.toString()}`);
 });
-
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
